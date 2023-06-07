df769b3ff9187655dff9aa2d977458c1cedf4299
OzLievano <Osvaldoalievano@gmail.com>
2023-06-07 18:05:53 -0400

    binary search algo

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,38 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  // search(arr, lo , hi)
+  // mid = lo + (hi-lo / 2)
+  // v = arr[m]
+
+  //if v === needle 
+  // return true
+
+  // if v > m ... go right 
+  // mid +=1 ?
+  // if v < m ... go left 
+  // mid -=1
+  let min = 0;
+  let max = haystack.length - 1;
+  let index;
+  let element;
+
+  while(min <= max) {
+    index = Math.floor((min+max) /2)
+    element = haystack[index];
+
+    if(element === needle) {
+      return true
+    }
+
+
+    if(element < needle) {
+      min = index + 1;
+    }
+    
+    if(element > needle) {
+      max = index - 1;
+    }
+  }
+
+  return false;
 
 }
